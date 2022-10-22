3c65bc2669f261ad35263ea7fa2472680dc74990
examanon <examanon@protonmail.com>
2022-10-22 14:34:37 +0200

    beta spreadsheet filter

--- beta/site.js
+++ beta/site.js
@@ -18,15 +18,24 @@ const openFile = fileId => {
 const sortBy = key => {
     const params = parseQS();
 
-    const isSortedBy = !params.sort.key || (params.sort.key === key);
+    const isSortedBy = !params.sort.key || params.sort.key === key;
     const isDefaultSort = !params.sort.dir;
 
-    const dir = !isSortedBy ? "ASC" : (params.sort.dir === "ASC" || isDefaultSort ? "DESC" : "ASC");
+    const dir = !isSortedBy ? "ASC" : params.sort.dir === "ASC" || isDefaultSort ? "DESC" : "ASC";
 
     params.sort.key = key;
     params.sort.dir = dir;
 
-    location.href = serializeParams(params);
+    location.href = serializeQS(params);
+};
+
+const filterBy = (key, value) => {
+    const params = parseQS();
+
+    params.filter.key = key;
+    params.filter.value = value;
+
+    location.href = serializeQS(params);
 };
 
 const openModal = () => {
@@ -61,8 +70,8 @@ const closeModal = () => {
 
 const parseQS = () => {
     const href = document.location.href;
-    const [, paramsStr] = href.split('?');
-    const params = (paramsStr || "").split('&');
+    const [, paramsStr] = href.split("?");
+    const params = (paramsStr || "").split("&");
 
     const paramsObj = params.reduce((acc, paramStr) => {
         const [key, value] = paramStr.split("=");
@@ -71,20 +80,26 @@ const parseQS = () => {
         return { ...acc, [key]: value };
     }, {});
 
-    console.log('params', paramsObj);
+    console.log("params", paramsObj);
 
     return {
         sort: {
             key: paramsObj?.sortKey || null,
-            dir: paramsObj?.sortDir || null
-        }
+            dir: paramsObj?.sortDir || null,
+        },
+        filter: {
+            key: paramsObj?.filterKey || null,
+            value: paramsObj?.filterValue || null,
+        },
     };
 };
 
-const serializeParams = (params) => {
+const serializeQS = params => {
     const paramObj = {
         sortKey: params.sort.key || null,
-        sortDir: params.sort.dir || null
+        sortDir: params.sort.dir || null,
+        filterKey: params.filter.key || null,
+        filterValue: params.filter.value || null,
     };
 
     const qsObj = Object.keys(paramObj).reduce((acc, key) => {
@@ -92,19 +107,57 @@ const serializeParams = (params) => {
         return [...acc, `${key}=${paramObj[key]}`];
     }, []);
 
-    const qs = qsObj.join('&');
+    const qs = qsObj.join("&");
 
-    return !qs ? '' : `?${qs}`;
+    return !qs ? "" : `?${qs}`;
 };
 
 const parseTableMetadata = () => {
     const metadata = {
-        columns: window.columns
+        columns: window.columns,
     };
 
-    console.log('metadata', metadata);
+    console.log("metadata", metadata);
 
     return metadata;
+};
+
+const filterTableRows = (params, metadata) => {
+    if (!params.filter.key) return;
+
+    const table = getTableElement();
+    const rows = Array.from(table.querySelectorAll("tbody tr"));
+
+    const key = params.filter.key;
+    const value = params.filter.value || "";
+
+    console.log("filter", key, value);
+
+    const addClearFilterBadge = () => {
+        const filterClear = document.createElement('div');
+
+        filterClear.innerHTML = '<a href="/beta">Clear filters</a>';
+        filterClear.classList.add("filter-clear");
+
+        document.querySelector('body').append(filterClear);
+    };
+
+    const filterRows = () => {
+        rows.forEach(tr => {
+            const cell = tr.querySelector(`td[data-key=${key}]`);
+            const text = (cell.textContent || "").trim().toLowerCase();
+            const pattern = value.trim().toLowerCase();
+
+            const isMatch = key === "characters" ? text.includes(pattern) : text === pattern;
+
+            if (!isMatch) {
+                tr.remove();
+            }
+        });
+    };
+
+    filterRows();
+    addClearFilterBadge();
 };
 
 const sortTableRows = (params, metadata) => {
@@ -115,27 +168,29 @@ const sortTableRows = (params, metadata) => {
     const key = params.sort.key || metadata.columns[firstSortableColumn].id;
     const dir = params.sort.dir || "ASC";
 
+    console.log("sort", key, dir);
+
     const addHeaderTriangle = () => {
-        const thead = table.querySelector('thead');
+        const thead = table.querySelector("thead");
         const th = thead.querySelector(`th[data-key=${key}]`);
 
-        th.textContent += ` ${dir === "ASC" ? '▲' : '▼'}`;
+        th.textContent += ` ${dir === "ASC" ? "▲" : "▼"}`;
     };
 
     const sortRows = () => {
-        const tbody = table.querySelector('tbody');
-        const rows = Array.from(tbody.querySelectorAll('tr'));
+        const tbody = table.querySelector("tbody");
+        const rows = Array.from(tbody.querySelectorAll("tr"));
 
         const values = rows.map((tr, index) => {
             const cell = tr.querySelector(`td[data-key=${key}]`);
 
             return {
                 index,
-                value: (cell.textContent || "").trim()
+                value: (cell.textContent || "").trim(),
             };
         });
 
-        values.sort((a, b) => a.value < b.value ? -1 : 1);
+        values.sort((a, b) => (a.value < b.value ? -1 : 1));
 
         if (dir === "DESC") {
             values.reverse();
@@ -150,8 +205,6 @@ const sortTableRows = (params, metadata) => {
         }
     };
 
-    console.log("sort", key, dir);
-
     sortRows();
     addHeaderTriangle();
 };
@@ -160,8 +213,6 @@ const init = () => {
     const metadata = parseTableMetadata();
     const params = parseQS();
 
+    filterTableRows(params, metadata);
     sortTableRows(params, metadata);
-
-    //TODO: filter table rows
-    //TODO: sort table rows
 };
